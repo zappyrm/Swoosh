ac3c84ae122b479173284811afaa8a832f307475
Romeu  Bonini Neto <romeuboninineto@iMac-de-Romeu.local>
2020-04-01 21:43:42 -0300

    arquivos alterados

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -3,12 +3,40 @@ package com.example.swoosh
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun onMensClicked(view: View){
+
+      womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "mens"
+
+    }
+    fun onWomensClicked(view: View){
+
+      mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "womens"
+
+    }
+    fun onCoedClicked(view: View){
+
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+
+        selectedLeague = "coed"
+
     }
 
     fun leagueNextClicked(view: View) {
